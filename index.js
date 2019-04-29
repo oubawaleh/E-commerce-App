397a33061869170b9725a6d91d91f0838dbbfe07
Oubah Awaleh <oubawaleh@gmail.com>
2019-04-28 19:14:09 -0700

    update project dev dependencies

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/heartbeat", (req, res) =>
   res.send({ Datetime: new Date().toJSON() })
