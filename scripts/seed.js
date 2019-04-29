678b2b7737fe73d979369ad2deda6ec0dfd07add
Oubah Awaleh <oubawaleh@gmail.com>
2019-04-28 19:11:01 -0700

    add sample data to feed db

---
+++ scripts/seed.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import { users, products } from "../scripts/data";
+import { UserModel } from "../models/User";
+import { ProductModel } from "../models/Product";
+
+mongoose.connect("mongodb://localhost:27017");
+const db = mongoose.connection;
+
+db.on("error", error => {
+  console.error(error);
+});
+
+db.once("open", () => {
+  console.log("Database connection is open!");
+  UserModel.insertMany(users, error => {
+    if (error) {
+      console.error(error);
+    }
+  });
+  ProductModel.insertMany(products, error => {
+    if (error) {
+      console.error(error);
+    }
+  });
+});
