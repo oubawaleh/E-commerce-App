7cc78e10257edc1201717cbaad09bcca6f3a77eb
Oubah Awaleh <oubawaleh@gmail.com>
2019-05-17 10:06:24 -0700

    fixed typo causing error in testing

--- middleware/withAuthentication.js
+++ middleware/withAuthentication.js
@@ -7,12 +7,10 @@ export default jwt({
     if (
       req.headers &&
       req.headers.authorization &&
-      req.headers.authorization.split(" ")[0] === "Bearer" //1
+      req.headers.authorization.split(" ")[0] === "Bearer"
     ) {
-      return req.headers.authorization.split(" ")[1]; // token//2
+      return req.headers.authorization.split(" ")[1];
     }
     return null;
   }
-
-  
 });
